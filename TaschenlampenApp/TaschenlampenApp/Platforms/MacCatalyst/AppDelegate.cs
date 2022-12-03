a8e100344ebb3e882b1fd95ad430c95f9f981046
Max Mannstein <max.mannstein@gmail.com>
2022-12-03 14:12:49 +0100

    Projektdateien hinzufügen.

---
+++ TaschenlampenApp/TaschenlampenApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace TaschenlampenApp;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
