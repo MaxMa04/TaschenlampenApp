9e1f6b5baaf4f908a80ad6037efd95629ce8052f
Max Mannstein <max.mannstein@gmail.com>
2022-12-04 13:03:30 +0100

    Basisfunktionen der App vollendet TODO: hinzufügen, dass die App im Hintergrund laufen kann

--- TaschenlampenApp/TaschenlampenApp/Views/MainPage.xaml.cs
+++ TaschenlampenApp/TaschenlampenApp/Views/MainPage.xaml.cs
@@ -1,5 +1,4 @@
 using System.Diagnostics;
-
 namespace TaschenlampenApp.Views;
 
 public partial class MainPage : ContentPage
@@ -11,37 +10,46 @@ public partial class MainPage : ContentPage
 		InitializeComponent();
         Accelerometer.ShakeDetected += Accelerometer_ShakeDetected;
 	}
-    private void Accelerometer_ShakeDetected(object sender, EventArgs e)
+    private async void Accelerometer_ShakeDetected(object sender, EventArgs e)
     {
         if(taschenlampeAn == false) 
         {
+            Accelerometer.Stop();
             hallo.Text = "Taschenlampe an";
+            await Flashlight.TurnOnAsync();
             taschenlampeAn = true;
             timer();
         }
         else
         {
+            Accelerometer.Stop();
             hallo.Text = "Taschenlampe aus";
+            await Flashlight.TurnOffAsync();
             taschenlampeAn = false;
             timer();
         }
         
     }
+    // Verhindert, dass die Taschenlampe direkt wieder aus/angeschaltet wird
     private async void timer()
     {
-        Accelerometer.Stop();
         stopWatch.Start();
-        await Task.Delay(2000);
-        if (stopWatch.ElapsedMilliseconds >= 2000)
+        await Task.Delay(4000);
+        if(stopWatch.ElapsedMilliseconds >= 4000)
         {
-            stopWatch.Reset();
             Accelerometer.Start(SensorSpeed.Game);
+
         }
+
     }
 
     protected override void OnAppearing()
     {
         base.OnAppearing();
-        Accelerometer.Start(SensorSpeed.Game);
+        if (Accelerometer.IsMonitoring == false)
+        {
+            Accelerometer.Start(SensorSpeed.Game);
+        }
+        
     }
 }
